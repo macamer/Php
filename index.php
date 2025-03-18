9becff53070451dd7a11e2998c048e6a76a52818
maria <marcavmer@alu.edu.gva.es>
2025-03-18 10:14:50 +0100

    add links

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
     <a href="02-conceptos.php">02-conceptos.php</a>
     <br>
     <a href="03-demo.php">03-demo.php</a>
+    <br>
+    <a href="03-demo2.php">03-demo2.php</a>
+    <br>
+    <a href="04-importfiles/index.php">04-importfiles.php</a>
 </h1>
 
 <style>
