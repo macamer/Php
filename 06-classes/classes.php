ccc83c9f50cc435f431fafa021f6beaf8c20cf0e
maria <marcavmer@alu.edu.gva.es>
2025-03-18 12:18:41 +0100

    promoted properties & array

--- 06-classes/classes.php
+++ 06-classes/classes.php
@@ -1,7 +1,7 @@
 <?php
 
 class SuperHero {
-    public $name;
+    /*public $name;
     public $powers;
     public $planet;
 
@@ -9,15 +9,31 @@ class SuperHero {
         $this->name = $name;
         $this->powers = $powers;
         $this->planet = $planet;
-    }
+    }*/
+
+    //promoted properties PHP 8 - ya se definen propiedades en el constructor
+    public function __construct(
+        public string $name, 
+        public array $powers, 
+        public string $planet,
+    ) {}
+    //readonly public string $name --> para evitar que se modifique
+    //private $name --> no se puede acceder al dato
+
 
     public function attack() {
         return "$this->name ataca con sus poderes!";
     }
 
     public function description() {
+
+        $powers = implode(", ", $this->powers);
+
         return "$this->name es un superhéroe que viene de
         $this->planet y tiene los siguientes poderes:
-        $this->powers";
+        $powers";
     }
 }
+
+$hero = new SuperHero("Superman", ["Volar", "Supervista", "Fuerza"], "Krypton");
+echo $hero->description();
